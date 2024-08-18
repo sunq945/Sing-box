b8d265f307bab1d63e872e75eda7b5b8306aee8c
sunq945 <sunq8964@gmail.com>
2024-08-18 14:13:10 +0800

    增加添加crotab自动设置定时检测选项，修复下载三个主程序失败的问题

--- sb_serv00.sh
+++ sb_serv00.sh
@@ -556,10 +556,10 @@ create_cron(){
 menu() {
    clear
    echo ""
-   purple "=== Serv00|ct8老王sing-box一键安装脚本 ===\n"
-   echo -e "${green}脚本地址：${re}${yellow}https://github.com/eooce/Sing-box${re}\n"
-   echo -e "${green}反馈论坛：${re}${yellow}https://bbs.vps8.me${re}\n"
-   echo -e "${green}TG反馈群组：${re}${yellow}https://t.me/vps888${re}\n"
+   purple "=== (Serv00|ct8) sing-box一键安装脚本 ===\n"
+   echo -e "${green}脚本地址：${re}${yellow}https://github.com/sun945/Sing-box${re}\n"
+   echo -e "原脚本地址：https://github.com/eooce/Sing-box\n"  
+
    purple "转载请著名出处，请勿滥用\n"
    green "1. 安装sing-box"
    echo  "========================="
